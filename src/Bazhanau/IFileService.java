10a9ce56ceb38b187df506a99e2f3b578b6d9535
frostiq <sayshayx@gmail.com>
2014-09-18 10:39:19 +0300

    moved some files

--- src/Bazhanau/IFileService.java
+++ src/Bazhanau/IFileService.java
@@ -2,7 +2,7 @@ package Bazhanau;
 
 import java.io.File;
 
-public abstract interface IFileService {
+public interface IFileService {
     public abstract String readFile(File paramFile);
 
     public abstract void writeText(File paramFile, String paramString);
