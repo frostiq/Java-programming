046f6c0f5d0dc28eb85a6bae0431799904d38d86
frostiq <sayshayx@gmail.com>
2015-02-26 20:49:50 +0300

    ClientWindow

--- Task8_RMI/src/Bazhanau/Task8/Client/ClientWindow.java
+++ Task8_RMI/src/Bazhanau/Task8/Client/ClientWindow.java
@@ -1,7 +1,18 @@
 package Bazhanau.Task8.Client;
 
-import Bazhanau.Task2.MainWindow;
+import javax.swing.*;
 
-public class ClientWindow extends MainWindow {
+public class ClientWindow extends JFrame {
+
+    protected final String[] ItemsColumnNames = new String[] {"Id", "Name", "Location"};
+    private JPanel controlPanel = new JPanel();
+    private String[] groupsColumnNames = new String[]{"Id", "Name", "Price", "Quantity", "Storage Id"};
+    private JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+    private JButton addButton = new JButton("Дадаць студэнта");
+    private JButton deleteButton = new JButton("Выдаліць абранага студэнта");
+
+    public static void main(String[] args) {
+        new ClientWindow();
+    }
 
 }
